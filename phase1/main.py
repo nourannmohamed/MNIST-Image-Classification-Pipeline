f451da6960ad5bfb64e271d9ac8e91b293086e56
nourannmohamed <nouranmohameda3@gmail.com>
2026-04-19 19:48:04 +0200

    main only has metrics

--- phase1/main.py
+++ phase1/main.py
@@ -1,65 +1,36 @@
-import numpy as np
-from tensorflow.keras.datasets import mnist
-from sklearn.model_selection import train_test_split
-from collections import Counter
-
-# =========================
-# LOAD DATA
-# =========================
-(X_train_full, y_train_full), (X_test_full, y_test_full) = mnist.load_data()
-
-X = np.concatenate([X_train_full, X_test_full], axis=0)
-y = np.concatenate([y_train_full, y_test_full], axis=0)
-
-print("Before preprocessing:", X.shape)
-
-# =========================
-# BINARY (0 vs ALL)
-# =========================
-y = (y == 0).astype(int)
-
-# =========================
-# NORMALIZE + FLATTEN
-# =========================
-X = X / 255.0
-X = X.reshape(X.shape[0], -1)
-
-# =========================
-# BALANCE DATASET
-# =========================
-idx_0 = np.where(y == 0)[0]
-idx_1 = np.where(y == 1)[0]
-
-np.random.seed(42)
-idx_0_down = np.random.choice(idx_0, size=len(idx_1), replace=False)
-
-idx = np.concatenate([idx_0_down, idx_1])
-X, y = X[idx], y[idx]
-
-print("Balanced dataset:", Counter(y))
-
-# =========================
-# TRAIN TEST SPLIT
-# =========================
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, stratify=y, random_state=42
-)
-
-# =========================
-# VARIANCE FILTER
-# =========================
-variances = np.var(X_train, axis=0)
-mask = variances > 0.01
-
-X_train = X_train[:, mask]
-X_test  = X_test[:, mask]
-
-print("After preprocessing:", X_train.shape)
-
-#testing the naive bayes model
+from preprocessing import load_and_preprocess
 from models.nb import GaussianNB
-model = GaussianNB()
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-from sklearn.metrics import accuracy_score
-print("Accuracy:", accuracy_score(y_test, y_pred))
+from models.perceptron import Perceptron
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+X_train, X_test, y_train, y_test = load_and_preprocess()
+
+models = {
+    "Naive Bayes": GaussianNB(),
+    "Perceptron": Perceptron()
+}
+
+results = {}
+
+for name, model in models.items():
+    print("\n" + "="*30)
+    print(f"Model: {name}")
+    print("="*30)
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    results[name] = acc
+
+    print(f"\nAccuracy: {acc:.4f}")
+
+    print("\nConfusion Matrix:")
+    print(confusion_matrix(y_test, y_pred))
+
+    print("\nClassification Report:")
+    print(classification_report(y_test, y_pred))
+
+print("\n=== Final Model Comparison ===")
+for model, acc in results.items():
+    print(f"{model}: {acc:.4f}")
